ba4c389f2e3b470fa0998dce1354584613b3482a
mariagriffin <maria.griffin@atu.ie>
2022-10-24 13:07:36 +0100

    Adding Hello World

---
+++ src/main/java/mavengithubactiondemo/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mavengithubactiondemo;
+
+/**
+ * @author Maria.Griffin
+ *
+ *Hello World
+ *
+ */
+public class App {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+		
+	}
+
+}
