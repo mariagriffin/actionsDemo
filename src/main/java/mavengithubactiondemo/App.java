67d2a428f2bb35dfc869a738a39d93277a122df8
mariagriffin <maria.griffin@atu.ie>
2022-10-24 13:39:04 +0100

    adding a new line

--- src/main/java/mavengithubactiondemo/App.java
+++ src/main/java/mavengithubactiondemo/App.java
@@ -14,7 +14,7 @@ public class App {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
-		System.out.println("adding to kick of a workflow");
+		System.out.println("adding to kick of a workflow today");
 		
 	}
 
