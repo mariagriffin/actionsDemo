73a89f72ce406172360b9921f5c6d032ae42de7c
Maria Griffin <maria.griffin@atu.ie>
2025-10-23 14:01:14 +0100

    Update App.java

--- src/main/java/mavengithubactiondemo/App.java
+++ src/main/java/mavengithubactiondemo/App.java
@@ -13,7 +13,7 @@ public class App {
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
+		System.out.println("Hello World!!");
 		System.out.println("adding to kick of a workflow today");
 		System.out.println("adding more code");	
 		System.out.println("adding changes");
