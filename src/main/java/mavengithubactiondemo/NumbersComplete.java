be015af6fd5fae3c849aea427ab4059d6a2678a2
Maria Griffin <maria.griffin@atu.ie>
2025-11-11 14:55:39 +0000

    Add multiply and divide methods to NumbersComplete

--- src/main/java/mavengithubactiondemo/NumbersComplete.java
+++ src/main/java/mavengithubactiondemo/NumbersComplete.java
@@ -14,6 +14,14 @@ public class NumbersComplete {
 		result = num1 - num2;
 		return result;
 	}
+
+		public static int multiply(int num1, int num2){
+		return num1 * num2;
+	}
+
+	public static int divide(int num1, int num2){ 
+		return num1 / num2;
+	}
 	
 	public static int largest(int num1, int num2){
 		if (num1 > num2)
