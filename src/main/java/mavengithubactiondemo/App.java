130c6285b45a253829a99efd09769be918fe59f0
Maria Griffin <maria.griffin@atu.ie>
2025-11-19 11:21:09 +0000

    Add extra print statements in App.java
    Added an additional print statement to the main method.

--- src/main/java/mavengithubactiondemo/App.java
+++ src/main/java/mavengithubactiondemo/App.java
@@ -15,7 +15,8 @@ public class App {
 	{
 		System.out.println("Hello World!!");
 		System.out.println("adding to kick of a workflow today");
-		System.out.println("adding more code");			
+		System.out.println("adding more code");	
+		System.out.println("adding even more code");		
 	}
 
 }
